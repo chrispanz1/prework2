2f8d8ae7cb0ae830f062a8b07a8d9d06f9aaa9c5
cpanz1 <23050438-cpanz1@users.noreply.replit.com>
2023-05-15 02:58:30 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+
+function tipValue(){
+  //first we select the bill amount from html//
+  let amount = document.getElementById('bill1').value;
+  /*Next we declare the constant variable tip as 0.2, in order to represent the tip percent in this case 20%*/
+const tipPercent=0.2;
+let tip=(amount*(tipPercent));
+        alert(tip)
+  //lastly we calculate the tip//
+}
+
+ 
